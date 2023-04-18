c5dbe0837efe998af823aa33facfc12862a83ca4
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-18 16:16:39 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+string senhaCorreta = "1234abcd" ;
+string senha;
+
+
+Console.Write("Digite a Senha de acesso: ");
+senha = Console.ReadLine()!;
+
+bool senhaAcesso = senha == senhaCorreta;
+
+if(!senhaAcesso)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("\nAcesso Negado !!");
+    Console.ResetColor();
+    Environment.Exit(-1);
+}
+ 
+ Console.WriteLine("\nAcesso Permitido !!");
+
+
